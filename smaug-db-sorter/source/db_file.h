71ef9630b8110ec5b9ceffcb9c7753fa1d013f65
waifro <77700999+waifro@users.noreply.github.com>
2021-12-21 15:29:30 +0000

    Delete smaug-db-sorter directory

--- smaug-db-sorter/source/db_file.h
+++
@@ -1,6 +0,0 @@
-#ifndef DB_FILE_H
-#define DB_FILE_H
-
-int DBFILE_CheckFile(char *filename);
-
-#endif
--- smaug-db-sorter/source/dir_operate.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIR_OPERATE_H
-#define DIR_OPERATE_H
-
-int DIR_OperativeTree(char *buffer);
-int DIR_IsFolder(char *pathname);
-int DIR_BlenchFolder(char *pathdir);
-
-#endif
